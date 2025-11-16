0c7fa7cccb1376891a43d78442354859a8b0a708
Vishu <youremail@example.com>
2025-11-16 08:31:06 +0100

    Initial deploy

---
+++ src/assets/images/index.js
@@ -0,0 +1,15 @@
+import hero from './hero.jpg';
+import mcdonalds from './mcdonalds.png';
+import picnic from './picnic.png';
+import motherson from './motherson.png';
+import siemens from './siemens.png';
+import logo from './logo.svg';
+
+export {
+ hero,
+ mcdonalds,
+ picnic,
+ motherson,
+ siemens,
+ logo,
+}
