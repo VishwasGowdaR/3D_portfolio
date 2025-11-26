a47e9f813ef689cc1e8fca6fed53b2460aa6002c
Vishu <youremail@example.com>
2025-11-26 15:53:01 +0100

    Deploy to GitHub Pages

---
+++ docs/assets/CTA-68f5f4c3.js
@@ -0,0 +1,1 @@
+import{j as s,L as t}from"./index-7710165d.js";const a=()=>s.jsxs("section",{className:"cta",children:[s.jsxs("p",{className:"cta-text",children:["Challenges fuel me. Opportunities inspire me. ",s.jsx("br",{}),"Let’s collaborate."]}),s.jsx(t,{to:"/contact",className:"btn",children:"Contact"})]});export{a as C};
